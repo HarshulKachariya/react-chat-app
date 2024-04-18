3fead9b80496444629d7c49f2c5df6eef8c0e990
HarshulKachariya <harshulkachariya17@gmail.com>
2024-04-18 17:30:03 +0530

    antd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import "./App.css";
 
-import { Button } from "antd";
+import { Button, Flex } from "antd";
 
 function App() {
   return (
     <>
       <h1>Bus booking</h1>
-      <Button type="primary">Book now</Button>
+      <Flex gap={20}>
+        <Button type="primary">Book now</Button>
+        <Button className="bg-black text-white ">Book now</Button>
+        <Button className="bg-gray-100 text-black ">Book now</Button>
+        <Button className="bg-gray-200 text-black ">Book now</Button>
+        <Button className="bg-gray-300 text-black ">Book now</Button>
+        <Button className="bg-gray-400 text-black ">Book now</Button>
+        <Button className="bg-gray-500 text-white ">Book now</Button>
+        <Button className="bg-gray-600 text-white ">Book now</Button>
+        <Button className="bg-gray-700 text-white ">Book now</Button>
+        <Button className="bg-gray-800 text-white ">Book now</Button>
+        <Button className="bg-gray-900 text-white ">Book now</Button>
+      </Flex>
     </>
   );
 }
